888488cc2a2fa3a1970d1707dad238427332aaad
Clara Prioux <prioux.clara@gmail.com>
2024-10-17 15:17:43 +0200

    User can see his words on profile page

--- backend/src/routes/profileRoutes.ts
+++ backend/src/routes/profileRoutes.ts
@@ -40,4 +40,51 @@ router.get("/getUser", async (req, res) => {
   }
 });
 
+router.get("/getWords", async (req, res) => {
+  try {
+    // Extract the user_id from the token
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "No token provided!" });
+    }
+
+    const decodedToken = jwt.verify(token, secretKey) as DecodedToken;
+    const user_id = decodedToken.id;
+    const objectIdUser = new mongoose.Types.ObjectId(user_id);
+
+    // Fetch user words (based on user_id)
+    const userWords = await mongoose.connection
+      .collection("userswords")
+      .find({ user_id: objectIdUser })
+      .toArray();
+
+    if (!userWords || userWords.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "User hasn't added any words yet!" });
+    }
+
+    // Extract word_ids from userWords to find corresponding word details
+    const wordIds = userWords.map((word) => word.word_id);
+
+    // Fetch actual word details from the words collection using the word_ids
+    const words = await mongoose.connection
+      .collection("words")
+      .find({
+        _id: { $in: wordIds.map((id) => new mongoose.Types.ObjectId(id)) },
+      })
+      .toArray();
+
+    if (!words || words.length === 0) {
+      return res.status(404).json({ message: "No words found for the user!" });
+    }
+
+    return res.status(200).json({ words });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error fetching user words", error });
+  }
+});
+
 export default router;
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Logout from "../components/LogoutBtn";
 
+type UsersWords = {
+  _id: string;
+  Word: string;
+  Translation: string;
+};
+
 const ProfilePage = () => {
-  const [user, setUser] = useState<{ username: string } | null>(null);
+  const [user, setUser] = useState<{
+    username: string;
+  } | null>(null);
+  const [usersWords, setUsersWords] = useState<UsersWords[]>([]);
 
   // Fetch the users
   const fetchUser = useCallback(async () => {
@@ -19,6 +28,7 @@ const ProfilePage = () => {
 
       const data = await res.json();
       setUser(data.userInfo);
+      fetchUsersWords();
     } catch (error) {
       console.error("Error fetching users", error);
     }
@@ -28,8 +38,28 @@ const ProfilePage = () => {
     fetchUser();
   }, [fetchUser]);
 
+  // Fetch the user's words
+  const fetchUsersWords = useCallback(async () => {
+    try {
+      const res = await fetch("http://localhost:3000/profile/getWords", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+
+      if (!res.ok) throw new Error("Error fetching users");
+
+      const data = await res.json();
+      setUsersWords(data.words);
+    } catch (error) {
+      console.error("Error fetching users", error);
+    }
+  }, []);
+
   return (
-    <div className="m-4 md:m-14 font-jost">
+    <div className="m-4 md:m-14 font-jost flex-1">
       <div className="flex justify-between">
         <h1 className="text-white text-2xl font-bold drop-shadow-lg">
           Hi {user ? user.username : "Loading..."}, <br /> Welcome to the
@@ -38,11 +68,28 @@ const ProfilePage = () => {
         <Logout />
       </div>
 
-      <div className="flex md:flex-row flex-col justify-center items-center md:space-x-36">
-        <div className="flex justify-center items-center bg-white rounded-full md:text-2xl text-lg w-36 md:w-56 h-36 md:h-56 md:mt-0 mt-10">
-          <p className="text-center">
-            92 <br /> words
-          </p>
+      <div className="flex md:flex-row flex-col justify-center items-center md:space-x-48">
+        <div className="mt-6 md:mt-10">
+          <div className="w-full h-96 overflow-y-auto bg-white rounded-md shadow-md mb-10">
+            <table className="w-80 text-black">
+              <tbody>
+                {usersWords.map((wordObj) => (
+                  <tr key={wordObj._id} className="border-b border-gray-100">
+                    <td className="p-2">{wordObj.Word}</td>
+                    <td className="p-1">{wordObj.Translation}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-10 mb-10 items-center">
+          <div className="flex justify-center items-center bg-white rounded-full shadow-xl md:text-2xl text-lg w-36 md:w-42 h-36 md:h-42 mt-0">
+            <p className="text-center">
+              92 <br /> words
+            </p>
+          </div>
+          <div className="py-32 px-56 bg-white shadow-lg"></div>
         </div>
       </div>
     </div>
