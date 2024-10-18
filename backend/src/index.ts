e0a159e5380845a5c5ce2752318c756943c4da7d
Clara Prioux <prioux.clara@gmail.com>
2024-10-18 11:00:31 +0200

    set up dev and prod env, fixes #32

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ connectDB().then(() => {
 // Allow requests from your frontend
 app.use(
   cors({
-    origin: "http://localhost:5173", // https://u09-memolang.netlify.app
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 
