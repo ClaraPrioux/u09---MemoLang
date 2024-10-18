562a14761a7620fc49a796205f69230907828324
Clara Prioux <prioux.clara@gmail.com>
2024-10-18 14:44:56 +0200

    trying to fix request deploy issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,24 @@
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import manifest from "./public/manifest.json";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: manifest,
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,svg,png,webp}"],
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.REACT_APP_API_URL": JSON.stringify(env.REACT_APP_API_URL),
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: manifest,
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,svg,png,webp}"],
+        },
+      }),
+    ],
+  };
 });
